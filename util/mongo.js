4c1524a447b173c40428b246f715dc61f5c47f8e
Marwan Mohamed <105984651+MarwanMohamedRefaei@users.noreply.github.com>
2023-01-08 18:38:28 +0200

    Update mongo.js

--- util/mongo.js
+++ util/mongo.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const MONGO_URL = mongodb+srv://mero:mero@cluster0.txopxwb.mongodb.net/restaurant-next?retryWrites=true&w=majority
+const MONGO_URL = `mongodb+srv://mero:mero@cluster0.txopxwb.mongodb.net/restaurant-next?retryWrites=true&w=majority`;
 
 if (!MONGO_URL) {
   throw new Error(
